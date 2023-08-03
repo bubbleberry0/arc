e9888e5ff1f410b18a8e6a437629b8564f62a5dd
bubbleberry0 <141344265+bubbleberry0@users.noreply.github.com>
2023-08-03 16:48:05 +0200

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,5 @@
+if game.GameId == 985731078 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f9722411e83fc15a226fb54eba1adbdb.lua"))()
+elseif game.GameId == 3846592040 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a86e1c63ee893e7d9c2804e5b4f13bf4.lua"))()
+end
