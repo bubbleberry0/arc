d8bbb652fb6f20715a642ddc8da2947af3f3a4c1
bubbleberry0 <141344265+bubbleberry0@users.noreply.github.com>
2023-08-25 18:00:51 +0200

    Peroxide

--- loader.lua
+++ loader.lua
@@ -4,4 +4,6 @@ elseif game.GameId == 3846592040 then
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a86e1c63ee893e7d9c2804e5b4f13bf4.lua"))()
 elseif game.GameId == 4096039463 then
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/33299472dc7b49fbdb6abfd4f0caac72.lua"))()
+elseif game.GameId == 3419284255 then
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e59daaad294311e2b8452ebddd998895.lua"))()
 end
